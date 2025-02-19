7cd323aeb0665155ae55c11cd95eefd43ac465ef
Samuel Almeida <samuelmk33@gmail.com>
2025-02-19 06:32:20 -0300

    first commit

---
+++ contato.php
@@ -0,0 +1,10 @@
+<?php
+    include_once("templates/header.php")
+?>
+
+    <h1>Página de Contato!</h1>
+    <h3>Salve!</h3>
+
+<?php
+    include_once("templates/footer.php")
+?>
---
+++ templates/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <p>Todos os direitos reservados &copy; 2025</p>
+</footer>
+</body>
+</html>
